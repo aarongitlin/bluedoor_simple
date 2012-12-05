244f54930614a57dfda911b6abf77d4d4ec58ee7
Aaron Gitlin <aarongitlin@gmail.com>
2012-12-05 18:59:06 +1100

    send thankyou email to sender

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,9 @@
+class Notifier < ActionMailer::Base
+  default :from => "aaron@lovesyoudesign.com"
+
+  # send a signup email to the user, pass in the user object that contains the user's email address
+  def thankyou_email(message)
+    mail( :to => user.email,
+    :subject => "Thanks for the enquiry" )
+  end
+end

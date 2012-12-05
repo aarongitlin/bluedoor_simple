32929d92261a1e04c676cce7eacb9cba279c68cf
Aaron Gitlin <aarongitlin@gmail.com>
2012-12-05 15:10:45 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BluedoorSimple::Application.config.secret_token = 'c793344b285e8c1e63b9748547a1125deefa93eae5ffb8d0e0bad20feccbb14265842c06c1f7cfe0e2e626f4af33e777896ff273aaeafed1f857ed309ed6e764'

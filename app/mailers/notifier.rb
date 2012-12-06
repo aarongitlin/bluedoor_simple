48a8a6da9202af272b1a08ac872d158c253998ff
Aaron Gitlin <aarongitlin@gmail.com>
2012-12-06 15:38:56 +1100

    woo got actionmailer up and running

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,8 +1,10 @@
 class Notifier < ActionMailer::Base
   default :from => "aaron@lovesyoudesign.com"
 
+
   # send a signup email to the user, pass in the user object that contains the user's email address
   def thankyou_email(message)
+    @setting = Setting.find(1)
     mail( :to => message.email,
     :subject => "Thanks for the enquiry" 
     )

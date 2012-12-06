2205a410b80acb8a5ad354ce166cf32ddb46affa
Aaron Gitlin <aarongitlin@gmail.com>
2012-12-06 13:59:30 +1100

    edited text for email to admin

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -4,6 +4,8 @@ class Notifier < ActionMailer::Base
   # send a signup email to the user, pass in the user object that contains the user's email address
   def thankyou_email(message)
     mail( :to => message.email,
-    :subject => "Thanks for the enquiry" )
+    :subject => "Thanks for the enquiry" 
+    :)
   end
+  def
 end
